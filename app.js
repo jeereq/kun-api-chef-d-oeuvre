ff62db7b7d0f8da3c57aa482c0237d0e007a00cc
jeereq <mingandajeereq@gmail.com>
2021-11-17 17:05:51 +0100

    feat integrate test static

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const graphqlHttp = require("express-graphql");
-const mongoose = require("mongoose");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-const schema = require("./graphql/resolvers');
+const schema = require('./graphql/resolvers');
 
-const is_auth = require("./middleware/is_auth");
+const is_auth = require('./middleware/is_auth');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res, next) => {
-	res.send("<h1> kun api par minganda ibanga jeereq</h1>");
+app.get('/', (req, res, next) => {
+	res.send('<h1> kun api par minganda ibanga jeereq</h1>);
 });
 
 //middleware d'authentification
 
-app.use("/", is_auth);
+app.use('/', is_auth);
 
 //graphql schema for all query and resolver
 
-app.use("/graphql", graphqlHttp({ schema, graphiql: true }));
+app.use('/graphql', graphqlHttp({ schema, graphiql: true }));
 
 //mongodb://localhost:27017/test_kun_api
 //mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.w82cr.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority
@@ -32,11 +32,11 @@ mongoose
 	.connect(`mongodb://localhost:27017/test_kun_api`, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
-		useCreateIndex: true
+		useCreateIndex: true,
 	})
 	.then(() => {
 		app.listen(`${process.env.PORT}`, () => {
-			console.log("running server on port " + process.env.PORT);
+			console.log('running server on port ' + process.env.PORT);
 		});
 	})
 	.catch((err) => {
